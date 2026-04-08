e0c15da4d5fac6278c489764975f1c17621f673a
Neutron-Pro <neutron@neutronstars.fr>
2026-04-08 21:33:18 +0200

    fix

--- src/main/java/fr/aimcvent/template/listener/player/JoinListener.java
+++ src/main/java/fr/aimcvent/template/listener/player/JoinListener.java
@@ -27,6 +27,7 @@ public class JoinListener implements Listener {
         event.setJoinMessage(null);
 
         final Player player = this.templateService.players().of(event.getPlayer());
+        player.sidebar().enabled(true);
         final Translation translation = player.aimcvent().translation();
 
         this.bukkitService.tabListOf(this.templateService)
